7f9608bcc33667a6867f5a698e7cc0b6bfb3c226
marcdtnguyen <86821559+marcdtnguyen@users.noreply.github.com>
2024-01-13 09:22:37 +1100

    Add js pseudocode

---
+++ script.js
@@ -0,0 +1,67 @@
+/*
+const expression = [0];
+const operations = ['+', '-', '*', '/']
+
+keyboard.addEventLister(keypress, onKey)
+
+window.addEventListener(click, onKey)
+
+onkey = (e) => {
+    key = processkey(e.target);
+    playanimation(key)
+    switch(key){
+        case number: addNumber();
+        break;
+        case op: addOperator();
+        break;
+        case equal: onEqual();
+    }
+}
+
+processkey = (target) => {
+    return key string value;
+}
+
+playanimation = (key) => {
+    playAnimation
+}
+
+addNumber = (num) => {
+    last = getLastValue()
+    if(last = 0){
+        expression.replace(last, num)
+    } else if (last = number){
+        expression.replace(last, last+num)
+    } else {
+        expression.push(num)
+    }
+}
+
+addOperator = (op) => {
+    last = getLastValue()
+    if(last = op){
+        expression.replace(last, op)
+    } else{
+        expression.push(op)
+    }
+}
+
+addition function (a, b) => a + b;
+subtraction function (a, b) => a - b;
+multiplication function (a, b) => a * b;
+division function (a, b) => a / b;
+
+onEqual = () => {
+    getExpression
+    value = calculate processed expression
+    display(value)
+}
+
+calculate = (proExp) {
+    proExp.reduce()
+    * / 
+    + -
+    return value;
+}
+
+*/
